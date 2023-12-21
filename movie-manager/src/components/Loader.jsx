a94b72184799c44355ab01a353e32ab6a661d07f
nayvok <44442827+nayvok@users.noreply.github.com>
2023-12-21 19:43:22 +0500

    Изменил дизайн лодера

--- movie-manager/src/components/Loader.jsx
+++ movie-manager/src/components/Loader.jsx
@@ -1,10 +1,10 @@
-import {Box, CircularProgress} from "@mui/material";
+import {Box, CircularProgress, LinearProgress} from "@mui/material";
 import React from "react";
 
 const Loader = () => {
     return (
-        <Box sx={{display:"flex", justifyContent: 'center', paddingTop: 10}}>
-            <CircularProgress size={60}/>
+        <Box sx={{width: "100%"}}>
+            <LinearProgress />
         </Box>
     );
 };
